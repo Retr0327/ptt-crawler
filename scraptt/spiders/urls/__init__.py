639654402453280cf0452bd3bae67cc11c6fe337
Retr0327 <philcoke35@gmail.com>
2022-08-06 02:23:04 +0800

    Add multiple request strategies

---
+++ scraptt/spiders/urls/__init__.py
@@ -0,0 +1,4 @@
+from .requests import AllRequestStrategy, EarliestRequestStrategy, RangeRequestStrategy
+
+
+__all__ = ["AllRequestStrategy", "EarliestRequestStrategy", "RangeRequestStrategy"]
---
+++ scraptt/spiders/urls/requests.py
@@ -0,0 +1,64 @@
+from scrapy import Request
+from typing import List, Callable
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+COOKIES = {"over18": "1"}
+
+
+class RequestStrategy(ABC):
+    """
+    The RequestStrategy builds the request urls.
+    """
+
+    @abstractmethod
+    def fetch(self):
+        pass
+
+
+@dataclass
+class AllRequestStrategy(RequestStrategy):
+    """
+    The AllRequestStrategy object fetches all the boards.
+    """
+
+    boards: List[str]
+    callback: Callable
+
+    def fetch(self):
+        for board in self.boards:
+            url = f"https://www.ptt.cc/bbs/{board}/index.html"
+            yield Request(url, cookies=COOKIES, callback=self.callback)
+
+
+@dataclass
+class EarliestRequestStrategy(RequestStrategy):
+    """
+    The EarliestRequestStrategy object fetches the earliest board.
+    """
+
+    boards: List[str]
+    callback: Callable
+
+    def fetch(self):
+        board = self.boards[0]
+        url = f"https://www.ptt.cc/bbs/{board}/index.html"
+        yield Request(url, cookies=COOKIES, callback=self.callback)
+
+
+@dataclass
+class RangeRequestStrategy(RequestStrategy):
+    """
+    The RangeRequestStrategy object fetches the board from a given range.
+    """
+
+    index_from: int
+    index_to: int
+    boards: List[str]
+    callback: Callable
+
+    def fetch(self):
+        board = self.boards[0]
+        for i in range(int(self.index_from), int(self.index_to) + 1):
+            url = f"https://www.ptt.cc/bbs/{board}/index{i}.html"
+            yield Request(url, cookies=COOKIES, callback=self.callback)
