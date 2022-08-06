37bf1803b666aca64a3fc98e6801ed9e353db8d8
Retr0327 <philcoke35@gmail.com>
2022-08-07 01:34:33 +0800

    Add YearRangeIndexParser

---
+++ scraptt/spiders/utils/parsers/page_index/__init__.py
@@ -0,0 +1,5 @@
+from .index import IndexParser
+from .latest_index import LatestIndex
+from .year_range import YearRangeIndexParser
+
+__all__ = ["IndexParser", "LatestIndex", "YearRangeIndexParser"]
