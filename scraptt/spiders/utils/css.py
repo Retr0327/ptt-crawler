16852a5bd206b14dbc820502116f1aa0aaec3e42
Retr0327 <philcoke35@gmail.com>
2022-08-07 01:01:23 +0800

    Update type hint

--- scraptt/spiders/utils/css.py
+++ scraptt/spiders/utils/css.py
@@ -1,14 +1,15 @@
 from typing import List
+from scrapy.selector import Selector
 from scrapy.http.response.html import HtmlResponse
 
 
-def get_title_tags(response: HtmlResponse) -> List[str]:
+def get_title_tags(response: HtmlResponse) -> List[Selector]:
     """The get_title_tags function gets the title tags DOM.
 
     Args:
         response (HtmlResponse): the scrapy HtmlResponse.
     Returns:
-        a list of anchor tags
+        a list of scrapy anchor tag selectors 
     """
     title_css = ".r-ent .title a"
 
