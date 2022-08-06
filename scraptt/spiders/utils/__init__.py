5978ba843c002b87c23ab3b4ae0299dae52eb85f
Retr0327 <philcoke35@gmail.com>
2022-08-07 01:02:05 +0800

    Add get_title_tags

---
+++ scraptt/spiders/utils/__init__.py
@@ -0,0 +1,8 @@
+from .request_strategies import (
+    AllRequestStrategy,
+    EarliestRequestStrategy,
+    RangeRequestStrategy,
+)
+
+from .css import get_title_tags
+from .parsers.page_index import LatestIndex
