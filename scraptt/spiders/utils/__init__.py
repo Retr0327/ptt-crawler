257fe7e1ed027f34dc7a781c98a20ef41370803a
Retr0327 <philcoke35@gmail.com>
2022-08-07 01:51:40 +0800

    Add get_post_info

--- scraptt/spiders/utils/__init__.py
+++ scraptt/spiders/utils/__init__.py
@@ -1,10 +1,10 @@
-from .css import get_title_tags
 from .request_strategies import (
     AllRequestStrategy,
     RangeRequestStrategy,
     YearRangeRequestStrategy,
 )
-from .parsers.page_index import LatestIndex
+from .parsers.page_index import LatestIndexParser
+from .css import get_title_tags, get_post_info
 
 
 __all__ = [
@@ -12,5 +12,6 @@ __all__ = [
     "RangeRequestStrategy",
     "YearRangeRequestStrategy",
     "get_title_tags",
-    "LatestIndex",
+    "get_post_info",
+    "LatestIndexParser",
 ]
