c4c827aa117c05582ed555f7bfed92220d1b5833
Retr0327 <philcoke35@gmail.com>
2022-08-09 00:48:06 +0800

    Update LatestIndexParser

--- scraptt/spiders/utils/parsers/page_index/latest_index.py
+++ scraptt/spiders/utils/parsers/page_index/latest_index.py
@@ -1,5 +1,7 @@
 import re
 from .base import Parser
+from scrapy import Request
+from typing import Callable
 from logging import LoggerAdapter
 from dataclasses import dataclass
 from .....constants import COOKIES
@@ -25,11 +27,14 @@ class LatestIndexParser(Parser):
     def get_board(self, url: str):
         return re.search(r"www\.ptt\.cc\/bbs\/([\w\d\-_]{1,30})\/", url).group(1)
 
-    def parse(self):
+    def parse(self, callback: Callable):
         latest_index = self.get_latest_index(self.prev_url)
         board = self.get_board(self.response.url)
         print("board", board)
         print("iindex", latest_index)
-        url = f"https://www.ptt.cc/bbs/{board}/index{latest_index}.html"
 
-        return
+        for index in range(1, latest_index + 1):
+            url = f"https://www.ptt.cc/bbs/{board}/index{index}.html"
+            self.logger.info(f"index link: {url}")
+
+            yield Request(url, cookies=COOKIES, callback=callback)
