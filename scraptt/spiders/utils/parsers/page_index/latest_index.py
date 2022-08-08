b1e3bb795448034407ed2ed922d5e83563859a06
Retr0327 <philcoke35@gmail.com>
2022-08-09 01:33:24 +0800

    Update LatestIndexParser

--- scraptt/spiders/utils/parsers/page_index/latest_index.py
+++ scraptt/spiders/utils/parsers/page_index/latest_index.py
@@ -14,12 +14,7 @@ class LatestIndexParser(Parser):
     The LatestIndexParser objects parses the latest index.html file.
     """
 
-    response: HtmlResponse
     logger: LoggerAdapter
-
-    def __post_init__(self) -> None:
-        self.prev_url = self.response.css('.btn.wide:contains("上頁")::attr(href)').get()
-        self.logger.info(f"index link: {self.prev_url}")
 
     def get_latest_index(self, prev_url: str):
         return int(re.search(r"index(\d{1,6})\.html", prev_url).group(1))
@@ -27,9 +22,13 @@ class LatestIndexParser(Parser):
     def get_board(self, url: str):
         return re.search(r"www\.ptt\.cc\/bbs\/([\w\d\-_]{1,30})\/", url).group(1)
 
-    def parse(self, callback: Callable):
-        latest_index = self.get_latest_index(self.prev_url)
-        board = self.get_board(self.response.url)
+    def parse(self, response: HtmlResponse, callback: Callable):
+        prev_url = response.css('.btn.wide:contains("上頁")::attr(href)').get()
+        self.logger.info(f"index link: {prev_url}")
+
+        latest_index = self.get_latest_index(prev_url)
+        board = self.get_board(response.url)
+
         print("board", board)
         print("iindex", latest_index)
 
