335b5cedbfc1efd5d1cfff8a79cd06143e7887f1
Retr0327 <philcoke35@gmail.com>
2022-08-08 19:04:42 +0800

    Add CommentsParser

---
+++ scraptt/spiders/utils/parsers/posts/__init__.py
@@ -0,0 +1,11 @@
+from .content import ContentCleaner
+from .comments import CommentsParser
+from .meta_data import get_meta_data, get_post_info
+
+
+__all__ = [
+    "ContentCleaner",
+    "CommentsParser",
+    "get_meta_data",
+    "get_post_info",
+]
