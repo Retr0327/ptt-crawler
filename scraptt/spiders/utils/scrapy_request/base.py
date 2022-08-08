c994395808ce31a2da47c66068b6d9382254837e
Retr0327 <philcoke35@gmail.com>
2022-08-08 14:38:40 +0800

    Add RequestStrategy

---
+++ scraptt/spiders/utils/scrapy_request/base.py
@@ -0,0 +1,14 @@
+from scrapy import Request
+from abc import ABC, abstractmethod
+from typing import Callable, Generator
+
+
+class RequestStrategy(ABC):
+    """
+    The RequestStrategy builds the request urls.
+    """
+
+    @abstractmethod
+    def fetch(self, callback: Callable) -> Generator[Request, None, None]:
+        """The fetch method fetches resources from a given url."""
+        pass
