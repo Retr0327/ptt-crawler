3a79f1b9fcad9a44b858906fae85207da13fdf16
Retr0327 <philcoke35@gmail.com>
2022-08-08 15:50:00 +0800

    Remove get_post_info

--- scraptt/spiders/utils/html_tag_helpers.py
+++ scraptt/spiders/utils/html_tag_helpers.py
@@ -1,6 +1,4 @@
-import re
 from typing import List
-from datetime import datetime
 from scrapy.selector import Selector
 from scrapy.http.response.html import HtmlResponse
 
@@ -16,24 +14,11 @@ def get_title_tags(response: HtmlResponse) -> List[Selector]:
     title_css = ".r-ent .title a"
 
     if response.url.endswith("index.html"):
-        prev_siblings = response.xpath(
-            '//*[@id="main-container"]/div[2]/div[10]/preceding-sibling::*'
-        )
-        return prev_siblings.css(title_css)
+        # prev_siblings = response.xpath(
+        #     '//*[@id="main-container"]/div[2]/div[10]/preceding-sibling::*'
+        # )
+        # return prev_siblings.css(title_css)
+        return response.dom(".r-list-sep").prev_all(title_css)
 
-    return response.css(title_css)
-
-
-def get_post_info(url: str) -> List[str]:
-    """The get_post_info function gets the info of a post from `url`.
-
-    Args:
-        url (str): the url of a post
-    Returns:
-        a list of post info (i.e. board, date, and post id)
-    """
-    board = re.search(r"www\.ptt\.cc\/bbs\/([\w\d\-_]{1,30})\/", url).group(1)
-    timestamp = re.search(r"(\d{10})", url).group(1)
-    date = datetime.fromtimestamp(int(timestamp)).strftime("%Y%m%d_%H%M")
-    post_id = url.split("/")[-1].split(".html")[0]
-    return [board, date, post_id]
+    # return response.css(title_css)
+    return response.dom(title_css)
