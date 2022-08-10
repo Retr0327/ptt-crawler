3d19e144d20f31cd387f12d0600ae5ab1eed1a56
Retr0327 <philcoke35@gmail.com>
2022-08-10 23:38:49 +0800

    Add BoardsSpider

---
+++ scraptt/spiders/boards.py
@@ -0,0 +1,15 @@
+from scrapy import Spider
+from scrapy.http.response.html import HtmlResponse
+
+
+class BoardsSpider(Spider):
+    """
+    The BoardsSpider object defines the behaviour for crawling all the boards from ptt.
+    """
+
+    name = "boards"
+    allowed_domains = ["ptt.cc"]
+    start_urls = ["https://www.ptt.cc/cls/1"]
+
+    def parse(self, response: HtmlResponse):
+        return
