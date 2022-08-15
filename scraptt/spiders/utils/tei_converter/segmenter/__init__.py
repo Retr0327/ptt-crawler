b390ce7aa9ade91b2097c62d3d3d3d9f4b73ac46
Retr0327 <philcoke35@gmail.com>
2022-08-15 17:31:48 +0800

    Add segment_text

---
+++ scraptt/spiders/utils/tei_converter/segmenter/__init__.py
@@ -0,0 +1,4 @@
+from .segmenter import segment_text
+
+
+__all__ = ["segment_text"]
---
+++ scraptt/spiders/utils/tei_converter/segmenter/segmenter.py
@@ -0,0 +1,38 @@
+import asyncio
+from typing import List, Union, Dict
+from .ckip_transformer import CKIPTransformer
+
+# --------------------------------------------------------------------
+# helper functions
+
+
+async def segment_comments(value):
+    return {**value, "content": CKIPTransformer([value["content"]]).transform()}
+
+
+async def segment_multiple_comments(data):
+    return await asyncio.gather(*list(map(segment_comments, data)))
+
+
+# --------------------------------------------------------------------
+# public interface
+
+
+def segment_text(data: List[Union[str, Dict[str, str]]]):
+    """The segment_text function segments the text.
+
+    Args:
+        data (list): the post-related data (i.e title, body or comments).
+    Returns:
+        a list of dicts in which the content is a list of list of tuples, a list of list of tuples
+        otherwise.
+    """
+    is_comments = all(isinstance(value, dict) for value in data)
+
+    if is_comments:
+        return asyncio.run(segment_multiple_comments(data))
+
+    if isinstance(data, str):
+        data = [data]
+
+    return CKIPTransformer(data).transform()
