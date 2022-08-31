45d431082fab0ed65cae4d9a395cdd431136b1d3
Retr0327 <philcoke35@gmail.com>
2022-09-01 01:12:18 +0800

    Import get_title_tags

--- scraptt/spiders/utils/__init__.py
+++ scraptt/spiders/utils/__init__.py
@@ -1,5 +1,4 @@
-from .post_data import get_post_data
-from .html_tag_helpers import get_title_tags
+from .html_helpers import get_title_tags
 
 
-__all__ = ["get_post_data", "get_title_tags"]
+__all__ = ["get_title_tags"]
